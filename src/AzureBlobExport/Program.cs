fbce496ff3c690f48001dc8256963516d236c7a3
Aleksandrs Maslovs <a.maslovs@gmail.com>
2017-03-14 10:33:33 +0200

    Added Start and End dates as input parameters

--- src/AzureBlobExport/Program.cs
+++ src/AzureBlobExport/Program.cs
@@ -6,8 +6,24 @@ namespace AzureBlobExport
     {
         static void Main(string[] args)
         {
-            var DATE_FROM = new DateTime(2017, 01, 01, 00, 00, 00);
-            var DATE_TO = new DateTime(2017, 01, 02, 00, 00, 00);
+            Console.Write("Please enter period start datetime in following format dd.MM.YYYY: ");
+            var startDate = Console.ReadLine();
+            Console.Write("Please enter period end datetime in following format dd.MM.YYYY: ");
+            var endDate = Console.ReadLine();
+
+            DateTime DATE_FROM = new DateTime();
+            DateTime DATE_TO = new DateTime();
+
+            try
+            {
+                DATE_FROM = DateTime.Parse(startDate);
+                DATE_TO = DateTime.Parse(endDate);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DateTime conversion failed. Please try following formats dd.MM.YYYY or dd/MM/YYYY");
+            }
+
             var copyFromAzure = new CopyFromAzure();
             copyFromAzure.Copy(DATE_FROM, DATE_TO);
 
