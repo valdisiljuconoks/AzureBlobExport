57cb3a421c45a623108d70ebf60f57856a8a7216
Aleksandrs Maslovs <a.maslovs@gmail.com>
2017-03-14 12:43:09 +0200

    Added support for Command-line arguments

--- src/AzureBlobExport/Program.cs
+++ src/AzureBlobExport/Program.cs
@@ -4,24 +4,41 @@ namespace AzureBlobExport
 {
     class Program
     {
+        public static string inputFrom;
+        public static string inputTo;
         static void Main(string[] args)
         {
-            Console.Write("Please enter period start datetime in following format dd.MM.YYYY: ");
-            var startDate = Console.ReadLine();
-            Console.Write("Please enter period end datetime in following format dd.MM.YYYY: ");
-            var endDate = Console.ReadLine();
-
             DateTime DATE_FROM = new DateTime();
             DateTime DATE_TO = new DateTime();
+            if (args.Length == 0)
+            {
+                InputVariables();
+                try
+                {
+                    DATE_FROM = DateTime.Parse(inputFrom);
+                    DATE_TO = DateTime.Parse(inputTo);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(
+                        "DateTime conversion failed. Please try following formats dd.MM.YYYY or dd/MM/YYYY");
+                    Main(new string[] {});
+                }
 
-            try
-            {
-                DATE_FROM = DateTime.Parse(startDate);
-                DATE_TO = DateTime.Parse(endDate);
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine("DateTime conversion failed. Please try following formats dd.MM.YYYY or dd/MM/YYYY");
+                try
+                {
+                    DATE_FROM = DateTime.Parse(args[0]);
+                    DATE_TO = DateTime.Parse(args[1]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(
+                        "DateTime conversion failed. Please try following formats dd.MM.YYYY or dd/MM/YYYY");
+                    Environment.Exit(0);
+                }
             }
 
             var copyFromAzure = new CopyFromAzure();
@@ -29,6 +46,15 @@ namespace AzureBlobExport
 
             Console.WriteLine("Done.");
             Console.ReadLine();
+        }
+
+
+        private static void InputVariables()
+        {
+            Console.Write("Please enter period start datetime in following format dd.MM.YYYY: ");
+            inputFrom = Console.ReadLine();
+            Console.Write("Please enter period end datetime in following format dd.MM.YYYY: ");
+            inputTo = Console.ReadLine();
         }
     }
 }
